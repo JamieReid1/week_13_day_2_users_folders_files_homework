4f0cdfe8a33fe953cbae8bf36e398af0cc23ce4b
Jamie Reid <wjreid85.jr@gmail.com>
2019-02-26 16:11:54 +0000

    Creates User class, passing tests.

---
+++ src/main/java/com/example/relationshipshomework/models/User.java
@@ -0,0 +1,21 @@
+package com.example.relationshipshomework.models;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/test/java/com/example/relationshipshomework/UserTest.java
@@ -0,0 +1,31 @@
+package com.example.relationshipshomework;
+
+import com.example.relationshipshomework.models.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+    User user;
+
+    @Before
+    public void before(){
+
+        user = new User("Dave");
+
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Dave", user.getName());
+    }
+
+    @Test
+    public void canSetName(){
+        user.setName("David");
+        assertEquals("David", user.getName());
+    }
+    
+}
